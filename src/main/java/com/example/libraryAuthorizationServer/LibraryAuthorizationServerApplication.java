52bc130f64e0ce38ff1f74a4f7f8333933ef6a3c
ITWeiss <gottfried98@bk.ru>
2025-06-02 18:16:08 +0700

    Initial commit

---
+++ src/main/java/com/example/libraryAuthorizationServer/LibraryAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryAuthorizationServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libraryAuthorizationServer/LibraryAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryAuthorizationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
